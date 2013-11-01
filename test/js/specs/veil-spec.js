3236341451d291f185262b5d38cbb32933115d54
Jan-Christian Foeh <jan@programmanstalt.de>
2013-11-01 18:37:29 +0100

    Copy the base structure from Attaché

--- test/js/specs/veil-spec.js
+++ test/js/specs/veil-spec.js
@@ -3,15 +3,105 @@
 describe("Veil", function() {
   "use strict";
 
-  var $anchor,
-      veil;
+  var veil;
 
   beforeEach(function() {
     
   });
 
   afterEach(function() {
-    
+    veil.destroy();
   });
 
+  it("should be lazy and not create markup on initialization", function() {
+    veil = new Veil();
+
+    expect($('.veil-overlay').length).toEqual(0);
+  });
+
+  it("should create popover markup on show()", function() {
+    veil = new Veil();
+    veil.show();
+
+    expect($('.veil-overlay').length).toEqual(1);
+  });
+
+  it("should mark popover as active on show()", function() {
+    veil = new Veil();
+    veil.show();
+
+    expect($('.veil-overlay').hasClass('inactive')).toBeFalsy();
+    expect($('.veil-overlay').hasClass('active')).toBeTruthy();
+  });
+
+  it("should mark popover as inactive on hide()", function() {
+    veil = new Veil();
+    veil.show();
+
+    veil.hide();
+
+    expect($('.veil-overlay').hasClass('active')).toBeFalsy();
+    expect($('.veil-overlay').hasClass('inactive')).toBeTruthy();
+  });
+
+  it("should not remove popover markup on hide()", function() {
+    veil = new Veil();
+    veil.show();
+
+    veil.hide();
+
+    expect($('.veil-overlay').length).toEqual(1);
+  });
+
+  it("should remove the popover from DOM when destroyed", function() {
+    veil = new Veil();
+    veil.show();
+    veil.destroy();
+
+    expect($('.veil-overlay').length).toEqual(0);
+  });
+
+  it("should update existing markup when setContent is called", function() {
+    veil = new Veil();
+    veil.setContent("foo");
+    veil.show();
+    veil.hide();
+
+    veil.setContent("bar");
+
+    expect($('.veil-overlay').text()).toEqual("bar");
+  });
+
+  describe("callbacks", function() {
+    it("should trigger the afterCreate callback after create, before show()", function() {
+      var overlayVisible  = true,
+          callbackFired   = false,
+          callback        = function(overlay) { overlayVisible = overlay.hasClass('active'); callbackFired = true; };
+
+      veil = new Veil({}, {afterCreate: callback});
+      veil.show();
+
+      expect(callbackFired).toBeTruthy();
+      expect(overlayVisible).toBeFalsy();
+    });
+
+    it("should trigger the afterShow callback after show()", function() {
+      var callbackFired = false,
+          callback      = function() { callbackFired = true; };
+
+      veil = new Veil({}, {afterShow: callback});
+      veil.show();
+
+      expect(callbackFired).toBeTruthy();
+    });
+  });
+
+  describe("options", function() {
+    it("should apply a provided CSS class to the popover", function() {
+      veil = new Veil({overlayClass: 'additional-class-test'});
+      veil.show();
+
+      expect(veil.overlay().hasClass('additional-class-test')).toBeTruthy();
+    });
+  });
 });
--- veil.js
+++ veil.js
@@ -16,9 +16,18 @@
    *
    * @class
    * @global
+   * @param {Object} [options]
+   * @param {String} [options.overlayClass] - additional CSS class(es) to apply to the overlay markup
+   * @param {Boolean} [options.debug=false] - provide debug information and error handling in the console
+   *
+   * @param {Object} [callbacks] - callbacks to be triggered at various lifecycle moments
+   * @param {afterCreateCallback} [callbacks.afterCreate] - called after the overlay markup has been created
+   * @param {afterShowCallback} [callbacks.afterShow] - called each time after the overlay is shown
    */
   function Veil(options, callbacks) {
     var that = this;
+
+    this.$overlay;
 
     this.options = $.extend({}, this.defaults, options);
 
@@ -38,16 +47,150 @@
     this.initialize();
   }
 
+  Veil.instances = [];
+
   Veil.prototype = (function() {
 
     var defaults,
+        initialize,
+        show,
+        hide,
+        destroy,
+        overlay,
+        exists,
+        isActive,
+        setContent,
         addCallback,
+        _createMarkup,
         _hasCallbackFor,
         _executeCallbacksFor,
         _debug;
 
     defaults = {
       debug: true
+    };
+
+    initialize = function initialize() {
+
+    };
+
+    /**
+     * Show the overlay. Create the markup first if it does not exist yet
+     *
+     * @memberOf Veil
+     * @public
+     */
+    show = function show() {
+      if ( !this.exists() ) {
+        _createMarkup.call(this);
+      }
+
+      // trigger a style recalculation in order to prevent the browser
+      // from coalescing the style changes from removing 'inactive' and
+      // adding 'active'. Coalescing the changes makes entry animations
+      // impossible, since the popover changes display from 'none' to 'block'
+      this.$overlay.removeClass('inactive');
+      this.$overlay.get(0).offsetHeight;
+      this.$overlay.addClass('active');
+
+      _executeCallbacksFor.call(this, 'afterShow', this.$overlay);
+    };
+
+    /**
+     * Hide the overlay. Create the markup first if it does not exist yet
+     *
+     * @memberOf Veil
+     * @public
+     */
+    hide = function hide() {
+      if (!this.isActive()) {
+        return false;
+      }
+
+      // trigger a style recalculation in order to prevent the browser
+      // from coalescing the style changes from removing 'active' and
+      // adding 'inactive'. Coalescing the changes makes exit animations
+      // impossible, since the popover changes display from 'block' to 'none'
+      this.$overlay.removeClass('active');
+      this.$overlay.get(0).offsetHeight;
+      this.$overlay.addClass('inactive');
+    };
+
+    /**
+     * A jQuery object of the overlay markup in the DOM
+     * 
+     * @memberOf Veil
+     * @public
+     * @returns {jQuery}
+     */
+    overlay = function overlay() {
+      return this.$overlay;
+    };
+
+    /**
+     * Does the overlay markup exist in the DOM?
+     *
+     * @memberOf Veil
+     * @public
+     * @returns {Boolean}
+     */
+    exists = function exists() {
+      return typeof this.$overlay !== 'undefined' && this.$overlay.length === 1;
+    };
+
+    /**
+     * Is this overlay currently active?
+     *
+     * @memberOf Veil
+     * @public
+     * @returns {Boolean}
+     */
+    isActive = function isActive() {
+      return this.exists() && this.$overlay.hasClass('active');
+    };
+
+    /**
+     * Modifies the content markup of the overlay
+     *
+     * @memberOf Veil
+     * @public
+     * @param {String} content
+     */
+    setContent = function setContent(content) {
+      this.content = content;
+
+      if (this.exists()) {
+        this.$overlay.html(this.content);
+      }
+    };
+
+    /**
+     * Hide the overlay, remove it from DOM and remove event handlers from the anchor element
+     *
+     * @memberOf Veil
+     * @public
+     */
+    destroy = function destroy() {
+      if ( !this.exists() ) {
+        return false;
+      }
+
+      this.$overlay.remove();
+    };
+
+    /**
+     * Create the popover markup in the DOM
+     *
+     * @memberOf Veil
+     * @private
+     */
+    _createMarkup = function _createMarkup() {
+      this.$overlay = $('<div class="veil-overlay inactive"></div>').addClass(this.options.overlayClass);
+
+      this.$overlay.html(this.content);
+
+      $('body').append(this.$overlay);
+      _executeCallbacksFor.call(this, 'afterCreate', this.$overlay);
     };
 
     /**
@@ -114,9 +257,35 @@
 
     return {
       defaults: defaults,
+      initialize: initialize,
+      show: show,
+      hide: hide,
+      destroy: destroy,
+      overlay: overlay,
+      exists: exists,
+      isActive: isActive,
+      setContent: setContent,
       addCallback: addCallback
     };
   })();
 
   return Veil;
 }));
+
+/**
+ * Callback called after the overlay markup has been created
+ *
+ * @callback afterCreateCallback
+ * @this Veil
+ *
+ * @param {jQuery} overlayElement
+ */
+
+/**
+ * Callback called after a overlay has been displayed
+ *
+ * @callback afterShowCallback
+ * @this Veil
+ *
+ * @param {jQuery} overlayElement
+ */
